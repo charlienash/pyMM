8242ad80a91c3d8badde328d5d44853887edc398
Charlie Nash <charlie.tc.nash@gmail.com>
2016-06-10 16:31:06 +0100

    E-step alternations for memory efficiency in progress.

--- demo.py
+++ demo.py
@@ -5,8 +5,8 @@ from util import _generate_mixture_data, plot_density
 
 def main():
     n_examples = 1000
-    data_dim = 100
-    n_components = 4
+    data_dim = 2
+    n_components = 2
     X = _generate_mixture_data(data_dim, n_components, n_examples)
 
     # Obscure data
@@ -15,24 +15,24 @@ def main():
     X_miss[r > 0.7] = np.nan
 
     # Initialize model
-#    gmm = GMM(n_components=50, robust=True)
-    gmm = SphericalGMM(n_components=13, robust=True)
+    gmm = GMM(n_components=n_components, robust=False)
+#    gmm = SphericalGMM(n_components=n_components, robust=True)
 #    gmm = DiagonalGMM(n_components=n_components)
 #    gmm = MPPCA(n_components=n_components, latent_dim=1)
 #    gmm = MFA(n_components=6, latent_dim=1, robust=False)
-    gmm = MFA(n_components=40, latent_dim=10, robust=True, SMALL=1e-6,
-              tol=1e-3)
+#    gmm = MFA(n_components=40, latent_dim=10, robust=True, SMALL=1e-6,
+#              tol=1e-3)
 #    gmm = MPPCA_Miss(n_components=n_components, latent_dim=2)
 #    gmm = MFA_Miss(n_components=n_components, latent_dim=1)
 
     # Fit GMM
-#    gmm.fit(X_miss, init_method='kmeans')
-    gmm.fit(X, init_method='kmeans')
-
+#    gmm.fit(X, init_method='kmeans')
+    gmm.fit(X_miss, init_method='kmeans')
+#
 #    print(gmm.score_samples(X))
 
     # Plot results
-#    plot_density(gmm, X=X, n_grid=50)
+    plot_density(gmm, X=X, n_grid=50)
 #    plt.savefig('test.png', dpi=600)
 
 main()
